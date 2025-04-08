e48916177921102413f1d2e0645c16a636c37222
naseeeeef <32449@yenepoya.edu.in>
2025-04-08 15:03:13 +0530

    Create networkscanner.py

---
+++ networkscanner.py
@@ -0,0 +1,17 @@
+
+import socket
+
+#called from imported package
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#for user can enter the input
+ipaddress = input("enter your ipaddress:")
+port = int(input("enter your port:"))
+
+#for define the exception
+def PortScanner(port):
+    if sock.connect_ex((ipaddress,port)):
+        print(f"PORT {port} is closed :)")
+    else:
+        print(f"PORT {port} is open (:")
+PortScanner(port)
